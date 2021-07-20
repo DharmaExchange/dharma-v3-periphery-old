2b0627b93f07cce5a2d780acffd0d0ab7fadfc1a
weidaix <weidai@inbox.ru>
2021-07-20 11:03:29 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Dharma V3 Periphery
+
+This repository contains the periphery smart contracts for the Dharma V3 Protocol.
+
+The corresponding [npm package](https://www.npmjs.com/package/dharma-v3-periphery) can be installed with:
+
+```
+npm install dharma-v3-periphery
+```
---
+++ deploy/1_deploy_router.ts
@@ -0,0 +1,22 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types"
+import { DeployFunction } from "hardhat-deploy/types"
+ 
+//WBNB 0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c
+//Factory 0x1b15f28630b875c4c83bfbfbfc9b230dec8c33b8
+//Router 0x90B3c58eeF196c6647EEB5957c5Ad003d3A1B643
+//Multicall2 0x397bdde000fe9bdcFEE2a3e6aCb03a1554c96B41
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts } = hre
+  const { deploy } = deployments
+  const { deployer } = await getNamedAccounts()
+
+  await deploy("SwapRouter", {
+    from: deployer,
+    args: ["0x1b15f28630b875c4c83bfbfbfc9b230dec8c33b8", "0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c"],
+    log: true,
+    skipIfAlreadyDeployed: true,
+  })
+}
+export default func
+func.tags = ["SwapRouter"]
